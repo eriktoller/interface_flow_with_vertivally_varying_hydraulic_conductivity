a4a652f7fb5cac2423b05d88dbc60c3382505d7f
eriktoller <54261045+eriktoller@users.noreply.github.com>
2019-08-19 08:36:15 +0200

    Delete Phi_arel_side.m

--- Phi_arel_side.m
+++
@@ -1,8 +0,0 @@
-function [ Phi ] = Phi_arel_side( z,z1,z2)
-%Complex potential for the side of an area-element
-nu=z2-z1;
-Z=(2*z-z1-z2)/nu;
-Lsq=nu*conj(nu);
-F=-(Z+1)*log(Z+1)+(Z-1)*log(Z-1)+2-2*log(.5*nu);
-Phi=Lsq/(64*pi*1i)*(conj(Z)-Z)*(F+conj(F));
-end
